b053a3fc3e09f0031ca4f7ec0cbedc6851377fd2
chris <cdwhelan@gmail.com>
2022-10-12 00:39:40 -0400

    add button and modal

---
+++ script/meerscript.js
@@ -0,0 +1,61 @@
+function load_content(content, placeholder, callback) {
+	console.log(content);
+	var xhr= new XMLHttpRequest();
+		xhr.open('GET', content, true);
+		xhr.onreadystatechange= function() {
+			if (this.readyState!==4) return;
+			if (this.status!==200) return; // or whatever error handling you want
+				document.getElementById(placeholder).innerHTML= this.responseText;
+				if(callback !== undefined){
+					console.log("calling");
+					callback();
+				}
+	}
+	xhr.send();
+	
+}
+
+
+function showNav(show){
+	console.log(show);
+	if(show)
+		document.getElementById("sidebar").style.display="block";
+	else
+		document.getElementById("sidebar").style.display="none";
+}
+
+
+function setModal(){
+		var elements = document.getElementsByClassName("modalbtn");
+		
+		for (var i = 0; i < elements.length; i++) {
+			elements[i].addEventListener('click', showModal, false);
+		}
+		
+		var elements = document.getElementsByClassName("close");
+		
+		for (var i = 0; i < elements.length; i++) {
+			console.log(i);
+			elements[i].addEventListener('click', closeModal, false);
+		}
+		
+	
+}
+function showModal() {
+	
+  console.log("modal");
+  let modal = document.getElementById("merrModal");
+  modal.style.display = "block";
+}
+
+window.onclick = function(event) {
+   let modal = document.getElementById("merrModal");
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+var closeModal = function() {
+	let modal = document.getElementById("merrModal");
+	modal.style.display = "none";
+}
